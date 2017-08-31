49f92e010d81ab9d17e20c901bb901484b72020b
yogoken <yogo.k@husky.neu.edu>
2017-08-31 13:05:58 +0900

    Add vue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <media :query="{maxWidth: 720}">smaller than 770px</media>
+    <vue-header></vue-header>
+    <time-line></time-line>
+  </div>
+</template>
+
+<script>
+  import Media from 'vue-media'
+  import VueHeader from './components/Header'
+  import TimeLine from './components/TimeLine'
+
+  export default {
+    components: {
+      Media,
+      VueHeader,
+      TimeLine
+    }
+  }
+</script>
